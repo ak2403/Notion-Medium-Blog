2777c4793f173ef599097202f0358a57e6ea563e
Arun Kumar <arunpasupathi24@gmail.com>
2021-05-21 10:47:42 +1000

    Function to retrieve the Table data.

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const { Client } = require("@notionhq/client");
-const { NotionKey } = require('./keys');
+const { NotionKey, NotionDB } = require('./keys');
 
 const Notion = new Client({ auth: NotionKey });
+
+const retriveDB = async (id) => {
+  const fetchData = await Notion.request({
+    path: 'databases/' + id + '/query',
+    method: 'POST',
+  })
+  return fetchData;
+}
+
+(async () => {
+
+  const fetchDBList = await retriveDB(NotionDB);
+
+})();
